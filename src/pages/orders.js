c1ec599b0e5be4e595e612a03c174e36c3f7bbe6
unknown <a.lee.intl@gmail.com>
2021-06-09 23:29:44 +0200

    June 9

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -1,7 +1,7 @@
 import { getSession, useSession } from "next-auth/client";
 import moment from 'moment';
 import db from "../../firebase";
-import Order from "./order";
+import Order from "./Order";
 
 const { default: Header } = require("../components/Header");
 
