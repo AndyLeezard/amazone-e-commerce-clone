577b3be23ec8d8fb15c1721d971ff4d0aa728d1e
unknown <a.lee.intl@gmail.com>
2021-06-16 16:02:44 +0200

    June 16_fix3

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -19,54 +19,6 @@ const firestore = (
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    Providers.Credentials({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      credentials: {
-        name: { label: "email", type: "email", placeholder: "test@test.test" },
-        password: { label: "pw", type: "password" }
-      },
-      async authorize(credentials, req) {
-        //const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // You need to provide your own logic here that takes the credentials
-        // submitted and returns either a object representing a user or value
-        // that is false/null if the credentials are invalid.
-        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // You can also use the `req` object to obtain additional parameters
-        // (i.e., the request IP address) 
-
-        console.log("credentials are :",credentials)
-        console.log(credentials.name)
-    
-          //is a new form
-          /*const makeSession = {
-              id : 1,
-              name : name,
-              email: email
-          }*/
-          const user  = JSON.stringify(credentials);
-          return user
-        
-        /*res.status(200).json(makeSession);
-
-        const res = await fetch("/api/custom-provider", {
-          method: 'POST',
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" }
-        })*/
-        //const user = await res.json(makeSession)
-        
-        // If no error and we have user data, return it
-        /*if (res.ok && user) {
-          return user
-        }
-        // Return null if user data could not be retrieved
-        return null*/
-      }
-    }),
     Providers.Google({
         clientId: process.env.GOOGLE_ID,
         clientSecret: process.env.GOOGLE_SECRET
